75f7fbe784b2974eb4e5fa86b2a4a934af4f7a29
asyousufmd-jpg <asyousufmd@gmail.com>
2026-07-20 14:26:24 +0530

    Add files via upload

---
+++ DefangIp.java
@@ -0,0 +1,20 @@
+gitpublic class DefangIp {
+    public String defangIPaddr(String address) {
+        StringBuilder result = new StringBuilder();
+        
+        
+        for (int i = 0; i < address.length(); i++) {
+            char current = address.charAt(i);
+            
+            
+            if (current == '.') {
+                result.append("[.]");
+            } else {
+                
+                result.append(current);
+            }
+        }
+        
+        return result.toString();
+    }
+}
